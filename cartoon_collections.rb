411ba940dbf6cf60d9bee7afc144765d39ea0dad
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-14 01:12:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+  arr.each_with_index{|val, index| puts "#{index+1}. #{val}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map{|value| value.capitalize() + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  index = 0 
+  return_value = true
+  while index < arr.length 
+    return_value = arr[index].length > 4 ? true : false
+    index += 1 
+  end
+  return_value
 end
 
 def find_the_cheese# code an argument here
